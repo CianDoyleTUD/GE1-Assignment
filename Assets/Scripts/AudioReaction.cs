f645fad2ba38f258cce487091f39f52458b86e81
CianDoyleTUD <C18430304@mytudublin.ie>
2021-12-12 13:50:30 +0000

    updated audioreaction + added lerp

--- Assets/Scripts/AudioReaction.cs
+++ Assets/Scripts/AudioReaction.cs
@@ -1,31 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AudioReaction : MonoBehaviour
 {
     public AudioSource source;
+    public float average = 0;
+    public float[] samples = new float[64];
+    private int bass = 8; 
+    private float multiplier = 100.0f; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine(AudioVisualsRoutine());
+    }
+
+    System.Collections.IEnumerator AudioVisualsRoutine()
+    {
+        while(true)
+        {
+            //transform.localScale = new Vector3(size, size, size);
+            yield return new WaitForSeconds(1);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float[] spectrum = new float[64];
+        samples = new float[64];
 
-        AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+        AudioListener.GetSpectrumData(samples, 0, FFTWindow.Blackman);
 
-        Debug.Log(spectrum[0].ToString() + " " + Mathf.Log(spectrum[0]));
+        List<float> subSamples = new List<float>();
 
-        float size = spectrum[0] * 50;
-        transform.localScale = new Vector3(size, size, size);
-        //transform.localScale = Vector3.Lerp (transform.localScale, new Vector3(size, size, size), 2.0f * Time.deltaTime);
-        /*for (int i = 1; i < spectrum.Length - 1; i++)
-        {
-            Debug.Log(spectrum[i].ToString() + " " + Mathf.Log(spectrum[i]));
-        }*/
+        for (int i = 0; i < bass; i++)
+        {   
+            subSamples.Add(samples[i]);
+        }
+        
+        Debug.Log(subSamples.Max().ToString());
+        float size = subSamples.Max() * 150;
+        transform.localScale = Vector3.Lerp (transform.localScale, new Vector3(size, size, size), 5.0f * Time.deltaTime);
     }
 }
