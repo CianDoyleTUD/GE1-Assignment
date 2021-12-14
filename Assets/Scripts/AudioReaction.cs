834299e60733e01be1cf5127dfab605155a1ce96
CianDoyleTUD <C18430304@mytudublin.ie>
2021-12-14 13:45:54 +0000

    Added some audio helper functions

--- Assets/Scripts/AudioReaction.cs
+++ Assets/Scripts/AudioReaction.cs
@@ -7,7 +7,7 @@ public class AudioReaction : MonoBehaviour
 {
     public AudioSource source;
     public ParticleSystem particle;
-    public float[] samples = new float[64];
+    private float[] samples = new float[64];
     private int bass = 8; 
     private float multiplier = 150.0f; // How much the audio affects the scale
     private float threshold = 5.0f;
@@ -26,6 +26,43 @@ public class AudioReaction : MonoBehaviour
         }
     }
 
+    // Returns a sub list of the subset of the given list between given indexes
+    float[] sliceFloatArray(float[] list, int startIndex, int endIndex)
+    {
+        float[] toReturn = new float[(endIndex - startIndex) + 1];
+        for(int i = startIndex; i <= endIndex; i++)
+        {
+            toReturn[i] = list[i];
+        }
+        return toReturn;
+    }
+
+    // Returns a dictionary containing the frequency values
+    Dictionary<string, float> getFrequencyValues(float[] list) 
+    {
+        int length = list.Length;
+        int div = length / 6;
+
+        // Divid the sample array into 6 portions
+        float subBass = sliceFloatArray(list, 0, div - 1).Max() * multiplier;
+        float bass = sliceFloatArray(list, div, div*2 - 1).Max() * multiplier;
+        float lowerMid =  sliceFloatArray(list, div*2, div*3 - 1).Max() * multiplier;
+        float mid = sliceFloatArray(list, div*3, div*4 - 1).Max() * multiplier;
+        float higherMid = sliceFloatArray(list, div*4, div*5 - 1).Max() * multiplier;
+        float prescence = sliceFloatArray(list, div*5, div*6 - 1).Max()* multiplier;
+
+        Dictionary<string, float> audioValues = new Dictionary<string, float>();
+
+        audioValues.Add("Sub bass", subBass);
+        audioValues.Add("Bass", bass);
+        audioValues.Add("Lower mid", lowerMid);
+        audioValues.Add("Mid", mid);
+        audioValues.Add("Higer mid", higherMid);
+        audioValues.Add("Presence", prescence);
+
+        return audioValues;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -33,14 +70,9 @@ public class AudioReaction : MonoBehaviour
 
         AudioListener.GetSpectrumData(samples, 0, FFTWindow.Blackman);
 
-        List<float> subSamples = new List<float>();
-
-        for (int i = 0; i < bass; i++)
-        {   
-            subSamples.Add(samples[i]);
-        }
+        Dictionary<string, float> audioValues = getFrequencyValues(samples);
         
-        float size = subSamples.Max() * multiplier;
+        float size = audioValues["Sub bass"];
         if (size > threshold)
         {
             ParticleSystem particleSystem = ParticleSystem.Instantiate<ParticleSystem>(particle, transform.position, transform.rotation);
@@ -48,7 +80,7 @@ public class AudioReaction : MonoBehaviour
             particleSystem.startSize = Mathf.Clamp(size, 0, 3.0f);
             var no = particleSystem.noise;
             no.strength =  Mathf.Clamp(size, 0, 2.0f);
-            Destroy(particleSystem, 2.5f);
+            Destroy(particleSystem, 1.0f);
         }
         transform.localScale = Vector3.Lerp (transform.localScale, new Vector3(size, size, size), 5.0f * Time.deltaTime);
     }
